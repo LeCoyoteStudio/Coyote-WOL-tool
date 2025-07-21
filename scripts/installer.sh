4db3e3628e247f7e1407b01fd4c6b3320b97a81a
Coyote Studio <contact@coyote.studio>
2025-07-21 18:13:07 +0200

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+# Rôle : Exécuté une seule fois lors de l'installation du paquet sur le NAS.
 
+echo "Coyote WOL Tool installation script."
+# Les dépendances sont incluses, aucune installation n'est nécessaire.
+# On s'assure juste que les scripts de gestion sont exécutables.
+chmod +x /var/packages/coyotewol/target/dsm_*.sh
+exit 0
